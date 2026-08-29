1331155290609ce8a165ca0d2ece88535c3d95ca
obob3010-lgtm <obob3010@gmail.com>
2026-08-29 22:30:41 +0500

    Update config.js

--- config.js
+++ config.js
@@ -17,8 +17,8 @@ window.MUSEUM_CONFIG = {
   maxMusicTracks: 100,
 
   // Atmosphere: use "auto" in normal mode. For testing: "winter" + "night", etc.
-  seasonMode: "Winter",
-  timeMode: "day",
+  seasonMode: "autumn",
+  timeMode: "sunset",
 
   // Optional built-in local soundtrack. Leave empty when using the cloud music library.
   musicTracks: []
